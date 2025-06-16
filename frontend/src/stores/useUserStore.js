d66d47217637db409b7eaa5d25b74849316ca5f1
aniketsingh <rajputbhaishaheb@gmail.com>
2025-06-16 23:39:16 +0530

    zustand

---
+++ frontend/src/stores/useUserStore.js
@@ -0,0 +1,30 @@
+import {create} from "zustand";
+import {toast} from "react-hot-toast"
+import axios from "../lib/axios.js"
+export const useUserStore=create ((set, get)=>({
+
+    user:null,
+    loading:false,
+    checkingAuth:true,
+
+    signup:async ({name,email,password,confirmPassword})=>{
+        set({loading:true});
+
+        if(password !== confirmPassword){
+            toast.error("Passwords do not match");
+            set({loading:false});
+            return;
+        }
+        
+        try {
+            const res=await axios.post("/auth/signup",{name,email,password});
+            set({user:res.data.user,loading:false});
+
+        } catch (error) {
+            set({loading:false});
+            toast.error(error.response.data.message|| "An error occured");
+        }
+   
+        
+    }}))
+    
