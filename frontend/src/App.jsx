9efe1602b1e514487d063866428172412c8c8796
aniketsingh <rajputbhaishaheb@gmail.com>
2025-06-16 22:38:15 +0530

    frontend sertup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import {Routes,Route} from 'react-router'
 
+
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignUpPage from './pages/SignUpPage.jsx'
+import Navbar from './components/Navbar.jsx'
 const App = () => {
   return (
-    <div className="text-blue-500"> 
-      heloo
+    <div className="min-h-screen bg-gray-900 text-white relative overflow-hidden"> 
+    {/* Background gradient */}
+			<div className='absolute inset-0 overflow-hidden'>
+				<div className='absolute inset-0'>
+					<div className='absolute top-0 left-1/2 -translate-x-1/2 w-full h-full bg-[radial-gradient(ellipse_at_top,rgba(16,185,129,0.3)_0%,rgba(10,80,60,0.2)_45%,rgba(0,0,0,0.1)_100%)]' />
+				</div>
+			</div>
+
+      {/* Navbar and Routes */}
+      <div className='relative z-50 pt-20'>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+      </Routes>
+      </div>
     </div>
   )
 }
